9c2af6a610f9607350598784b9e2354b4498efba
Goagel <146516536+Goagel@users.noreply.github.com>
2023-10-07 09:39:59 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -33,12 +33,20 @@ class MainWin(QWidget):
     def initUI(self):
         self.btn_next = QPushButton(txt_next)
         self.hello_text = QLabel(txt_hello)
-        #self.instruction = ...
+        self.instruction = QLabel('This application allows you to use the Rufier test to make an initial diagnosis of your health.\n'
+                    'The Rufier test is a set of physical exercises designed to assess your cardiac performance during physical exertion.\n'
+                    'The subject lies in the supine position for 5 minutes and has their pulse rate measured for 15 seconds; \n'
+                    'then, within 45 seconds, the subject performs 30 squats.\n'
+                    'When the exercise ends, the subject lies down and their pulse is measured again for the first 15 seconds\n'
+                    'and then for the last 15 seconds of the first minute of the recovery period.\n'
+                    'Important! If you feel unwell during the test (dizziness,\n'
+                    'tinnitus, shortness of breath, etc.), stop the test and consult a physician.' )
+
 
         self.layout_line = QVBoxLayout()
         self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
-        #adauga in panou Qlabelul instruction aliniat la stanga
-        #adauga in panou Qbutonul next aliniat pe centru      
+        self.layout_line.addWidget(self.instruction, alignment = Qt.AlignRight)
+        self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)      
         self.setLayout(self.layout_line)
 
     '''functia care este declansata la apasarea butonului next'''
