8977a1e295b7656324db9a09268850e56e8227ff
razvann12 <146516816+razvann12@users.noreply.github.com>
2023-10-14 10:09:40 +0300

    Update second_win.py

--- second_win.py
+++ second_win.py
@@ -118,10 +118,21 @@ class TestWin(QWidget):
         self.fw = FinalWin(self.exp)
 
     def timer_test1(self):
-        ''' '''
+        global time
+        time = QTime(0, 0, 15)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer1Event)
+        self.timer.start(1000)
 
     def timer1Event(self):
-        ''' '''
+       global time
+       time = time.addSecs(-1)
+       self.text_timer.setText(time.toString("hh:mm:ss"))
+       self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+       self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+       if time.toString("hh:mm:ss") == "00:00:00":
+           self.timer.stop()
+
 
     def timer2Event(self):
         ''' '''
@@ -130,14 +141,23 @@ class TestWin(QWidget):
         ''' '''
 
     def timer3Event(self):
-        ''' '''
+        ''''''
+        # if int(time.toString("hh:mm:ss")[6:8]) >= 45
+        #     self.text_timer.setStyleSheet("color: rgb(0,255,0)")
+        # elif int(time.toString("hh:mm:ss")[6:8]) >= 15
+        #     self.text_timer.setStyleSheet("color: rgb(0,255,0)")
+        # else:
+        #     self.text_timer.setStyleSheet("color: rgb(0,0,0)")
 
     def timer_final(self):
-        ''' '''
+        time = QTime(0, 1, 0)
+        self.timer.timeout.connect(self.timer3Event)
 
     def connects(self):
         '''conectati butoanele btn_next,   btn_test1,   btn_test2, btn_test3
         la functiile asociate  next_click, timer_test1, timer_bob, timer_final'''
+        self.btn_test1.clicked.connect(self.timer_test1)
+
 
     ''' sets what the window will look like (label, size, location) '''
     def set_appear(self):
